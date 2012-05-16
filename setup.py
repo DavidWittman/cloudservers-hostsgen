2aaafd6bfed903e337db4c3e1ba2da190f9afec3
David Wittman <david@wittman.com>
2012-05-15 22:21:21 -0500

    setup.py cleanup

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 __author__ = "David Wittman <david@wittman.com>"
 NAME = "cloudservers-hostsgen"
 DESC = "Auto-generates /etc/hosts for Rackspace Cloud Servers"
 VERSION = "1.0"
-REQS = [ 'python-novaclient==2.6.0' ]
+REQS = ['python-novaclient==2.6.0']
 
-setup(name = NAME,
-      description = DESC,
-      version = VERSION,  
-      author = "David Wittman",
-      author_email = "david@wittman.com",
-      license = "BSD",
-      install_requires = REQS,
-      py_modules = ['hostsgen'],
-      entry_points = """
-        [console_scripts]
-        cloudservers-hostsgen = hostsgen:main
-        """
-     )
+setup(
+    name = NAME,
+    description = DESC,
+    version = VERSION,
+    author = "David Wittman",
+    author_email = "david@wittman.com",
+    license = "BSD",
+    install_requires = REQS,
+    py_modules = ['hostsgen'],
+    entry_points = {
+        "console_scripts": ["cloudservers-hostsgen = hostsgen:main"]
+    }
+)
